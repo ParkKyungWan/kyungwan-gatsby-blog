9a6ec943f7679db29501e2280b8c940bf8b55a1e
ParkKyungWan <pkw8117197@gmail.com>
2025-12-22 23:18:00 +0900

    feat/홈화면에 posts추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,66 @@
+import { graphql } from 'gatsby';
 import React from 'react';
 
+import Banner from '../components/Banner';
+import PostList from '../components/PostList';
+import Section from '../components/Section';
 import Seo from '../components/Seo';
 import Layout from '../layout';
-import Banner from '../components/Banner';
+import PostClass from '../models/post';
+import { AllMarkdownRemark } from '../type';
 
 type HomeProps = {
   location: Location;
+  data: {
+    allMarkdownRemark: AllMarkdownRemark;
+  };
 };
 
-const Home: React.FC<HomeProps> = ({ location }) => {
+const Home: React.FC<HomeProps> = ({ location, data }) => {
+  const allPosts = data.allMarkdownRemark.edges.map(({ node }) => new PostClass(node));
+  const posts = allPosts.slice(0, 3);
   return (
     <>
-    <Banner/>
-    <Layout location={location} hasBanner={true}>
-      <Seo title='경완' description="KyungWan's Blog"/>
-    </Layout>
+      <Banner />
+      <Layout location={location} hasBanner={true}>
+        <Seo title='경완' description="KyungWan's Blog" />
+
+        {/* 게시글 리스트 */}
+        <Section
+          header={{
+            emoji: '🏃‍♂️',
+            kr: '게시글',
+            en: 'Posts',
+            more: <a href='/posts'>전체보기</a>,
+          }}
+        >
+          <PostList posts={posts} />
+        </Section>
+      </Layout>
     </>
   );
 };
 
 export default Home;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: frontmatter___date, order: DESC }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 500, truncate: true)
+          frontmatter {
+            categories
+            title
+            emoji
+            date(formatString: "YYYY.MM.DD")
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
