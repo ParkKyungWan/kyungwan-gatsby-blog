e6ac0066c45333e97a1c3eaccb9443baf4cc15e3
ParkKyungWan <pkw8117197@gmail.com>
2026-02-15 04:42:41 +0900

    design/포스트내용_좌우패딩12px추가

--- src/templates/post-template/styled.ts
+++ src/templates/post-template/styled.ts
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled';
 
+export const Wrapper = styled.div`
+  padding: 0 12px;
+`;
+
 export const PostContent = styled.div`
   display: flex;
   flex-direction: column;
